b24665af5f9d50ef9fb1d783203fb1061188c418
Leonov Alexandr <leongl@ya.ru>
2025-04-25 13:21:29 +0300

    Add files via upload

---
+++ kernels/onescript/onescript_kernel.py
@@ -0,0 +1,86 @@
+import subprocess
+import tempfile
+from ipykernel.kernelbase import Kernel
+import logging
+
+class OneScriptKernel(Kernel):
+    implementation = 'OneScript'
+    implementation_version = '1.0'
+    language = 'onescript'
+    language_version = '1.0'
+    language_info = {
+        'name': 'OneScript',
+        'mimetype': 'text/plain',
+        'file_extension': '.os',
+    }
+    banner = "OneScript kernel for Jupyter Notebook"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.log = logging.getLogger(__name__)
+        self.log.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+        self.log.addHandler(handler)
+        print("init qqq")
+        self.log.info("Initializing qqq")
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
+        # Создаем временный файл для хранения кода
+        if code[0:4]=="opm ":
+            # Установка пакетов пока не работает - нужно разбираться
+            comand=code
+            params=''
+        else:
+            # Создаем временный файл для хранения кода
+            with tempfile.NamedTemporaryFile(suffix='.os', mode='w', delete=False) as temp_file:
+                temp_file.write(code)
+                temp_file_path = temp_file.name
+
+            comand='oscript'
+            params=temp_file_path
+
+            
+        
+        try:
+            # Выполняем файл через OneScript
+            result = subprocess.run(
+                [comand, params],
+                capture_output=True,
+                text=True
+            )
+
+            # Читаем вывод и ошибки
+            output = result.stdout.strip()
+            error = result.stderr.strip()
+
+            if error:
+                stream_content = {'name': 'stderr', 'text': error}
+                self.send_response(self.iopub_socket, 'stream', stream_content)
+            else:
+                stream_content = {'name': 'stdout', 'text': output}
+                self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        finally:
+            # Удаляем временный файл после выполнения
+            import os
+            try:
+                os.unlink(temp_file_path)
+            except Exception as e:
+                self.log.info(f"Failed to delete temporary file: {e}")
+
+        return {
+            'status': 'ok',
+            'execution_count': self.execution_count,
+            'payload': [],
+            'user_expressions': {},
+        }
+
+    def do_shutdown(self, restart):
+        print("shutdown")
+        self.log.info("Shutting down qqq")
+        pass  # Нет процессов, которые нужно завершать
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=OneScriptKernel)
